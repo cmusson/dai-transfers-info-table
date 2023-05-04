5bc14ab4d7253ccf955cc6e70e630b1defc232d7
Connor <mussonconnor@gmail.com>
2023-05-03 20:22:01 -0600

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,15 @@
-import Web3 from "web3";
 import Table from "./Table";
-
-interface ITransfer {
-  blockNumber: number;
-  amount: number;
-  sender: string;
-  recipient: string;
-  transactionHash: string;
-}
-
-const infuraApi = "0e29589be8ee406fbdd8ff9cd65788e2";
-const infuraUrl = `https://mainnet.infura.io/v3/${infuraApi}`;
-const web3 = new Web3(infuraUrl);
-const daiAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
-const ETHERSCAN_API_KEY = "NB64ZPT1ZABJHCRRG1CAGPZ4MWGIPYZM33";
-const ETHERSCAN_API_URL = `https://api.etherscan.io/api?module=contract&action=getabi&address=0x6b175474e89094c44da98b954eedeac495271d0f&apikey=${ETHERSCAN_API_KEY}`;
-
-const getTimestamp = async (blockNumber: string) => {
-  const timestamp = (await web3.eth.getBlock(blockNumber)).timestamp.toString();
-  return new Date(Number(timestamp) * 1000).toLocaleTimeString();
-};
-
-async function getTransfers() {
-  const res = await fetch(ETHERSCAN_API_URL, { cache: "no-store" });
-  const data = await res.json();
-  const daiABI = JSON.parse(data.result);
-  const daiContract = new web3.eth.Contract(daiABI, daiAddress);
-  const blockNumber = await web3.eth.getBlockNumber();
-  const fromBlock = Math.max(0, blockNumber - 200);
-  const daiTransfers = await daiContract.getPastEvents(
-    "Transfer",
-    {
-      fromBlock: fromBlock,
-      toBlock: "latest",
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-  const dai100 = daiTransfers.slice(0, 100);
-  // console.log("100 --->", dai100);
-  // Map the transfer events to a new array of objects with the required data
-  const transfers = dai100.map((event) => {
-    return {
-      blockNumber: event.blockNumber.toString(),
-      amount: event.returnValues.wad as number,
-      sender: event.returnValues.src as string,
-      recipient: event.returnValues.dst as string,
-      transactionHash: event.transactionHash,
-    };
-  });
-  // console.log("TRANSFERS =====> ", transfers);
-  // Return the array of transfers
-  for (const tran of transfers) {
-    // console.log(tran);
-    const timestamp = await getTimestamp(tran.blockNumber);
-    tran.blockNumber = `${timestamp}`;
-  }
-  return transfers as unknown as ITransfer[];
-}
+import Image from "next/image";
+import { fetchPastTransfers } from "./api/getTransfers/fetchPastTransfers";
 
 export default async function Home() {
-  const transfers = await getTransfers();
+  const transfers = await fetchPastTransfers();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Last 100 Dai Transfers</h1>
+    <main className="flex min-h-screen flex-col items-center  ">
+      <header className="flex justify-center tablet:justify-start pl-2 h-20 sticky top-0 left-0 right-0 z-10 w-full select-none border-b-2 border-solid border-prpl-row bg-black/[0.6] backdrop-blur">
+        <Image src={"/Tessera.svg"} alt={""} width={128} height={25} />
+      </header>
+
       <Table transfers={transfers} />
     </main>
   );
